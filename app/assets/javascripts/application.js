2b202c2830472282649983711a6704bb860ffbcb
Jordan Skarbek <jaskarbek@gmail.com>
2013-07-30 20:20:30 -0700

    actually angular require to the load order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require angular
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
