89086e6350d1be65a6a67731ccf39655b8eefecc
Jordan Skarbek <jaskarbek@gmail.com>
2013-08-03 20:19:28 -0700

    wiped out old generated hero controller files

--- app/assets/javascripts/app.js
+++
@@ -1,7 +0,0 @@
-window.app = angular.module('Dotameter', []);
-
-app.config([
-  "$httpProvider", function($httpProvider) {
-    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
-  }
-]);
