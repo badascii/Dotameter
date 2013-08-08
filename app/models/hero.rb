9ecb9f4ab653a131ac85b5aa3387db43154823db
theoretick <mail@limitlesschannels.com>
2013-08-08 11:02:01 -0700

    renamed detailed_matches to matches, updated rake task and added scaffolding for model creation

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -31,41 +31,48 @@ class Hero
   validates :agi, presence: true, numericality: true
   validates :int, presence: true, numericality: true
 
-  def self.get_hero_data
-    hero_stats = []
-    File.open('hero_stats/hero_search.txt', 'r').each_line do |heroes|
-      hero_stats << heroes
+  def self.get_raw_hero_data
+    File.read('hero_stats/hero_search.txt').each_line do |heroes|
+      hero_stats = heroes
     end
     hero_stats
   end
 
+def self.get_json_hero_data
+  # returns parsed json
+end
+
+
   def self.build_heroes
-    heroes = Hero.get_hero_data
+    heroes = Hero.get_json_hero_data
     heroes.each do |hero|
+      puts hero
+      puts hero.class
+      puts hero.first
       Hero.create!(name: hero.keys.first,
-                   radiant_team:     hero[:radiant_team],
-                   primary_stat:     hero[:primary_stat],
-                   str:              hero[:str],
-                   agi:              hero[:agi],
-                   int:              hero[:int],
-                   str_per_lvl:      hero[:str_per_lvl],
-                   agi_per_lvl:      hero[:agi_per_lvl],
-                   int_per_lvl:      hero[:int_per_lvl],
-                   hp:               hero[:hp],
-                   mp:               hero[:mp],
-                   min_dmg:          hero[:min_dmg],
-                   max_dmg:          hero[:max_dmg],
-                   armor:            hero[:armor],
-                   move_speed:       hero[:move_speed],
-                   turn_rate:        hero[:turn_rate],
-                   day_sight:        hero[:day_sight],
-                   night_sight:      hero[:night_sight],
-                   attack_range:     hero[:attack_range],
-                   front_swing:      hero[:front_swing],
-                   back_swing:       hero[:back_swing],
-                   front_cast_time:  hero[:front_cast_time],
-                   back_cast_time:   hero[:back_cast_time],
-                   base_attack_time: hero[:base_attack_time],
+                   radiant_team:     hero.first[:radiant_team],
+                   primary_stat:     hero.first[:primary_stat],
+                   str:              hero.first[:str],
+                   agi:              hero.first[:agi],
+                   int:              hero.first[:int],
+                   str_per_lvl:      hero.first[:str_per_lvl],
+                   agi_per_lvl:      hero.first[:agi_per_lvl],
+                   int_per_lvl:      hero.first[:int_per_lvl],
+                   hp:               hero.first[:hp],
+                   mp:               hero.first[:mp],
+                   min_dmg:          hero.first[:min_dmg],
+                   max_dmg:          hero.first[:max_dmg],
+                   armor:            hero.first[:armor],
+                   move_speed:       hero.first[:move_speed],
+                   turn_rate:        hero.first[:turn_rate],
+                   day_sight:        hero.first[:day_sight],
+                   night_sight:      hero.first[:night_sight],
+                   attack_range:     hero.first[:attack_range],
+                   front_swing:      hero.first[:front_swing],
+                   back_swing:       hero.first[:back_swing],
+                   front_cast_time:  hero.first[:front_cast_time],
+                   back_cast_time:   hero.first[:back_cast_time],
+                   base_attack_time: hero.first[:base_attack_time],
                    )
     end
   end
