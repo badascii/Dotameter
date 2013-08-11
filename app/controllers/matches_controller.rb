73caeb9d8c13af5dce4710ca31796168b74c4e71
theoretick <me@lucascharles.me>
2013-08-10 22:17:36 -0700

    added matches ng-controller, index and show pages

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,25 @@
-  def show
-    @match = Match.find(params[:id])
-  end
+class MatchesController < ApplicationController
 
   def index
-    @matches = Match.paginate(page: params[:page], per_page: 50)
+    # @matches = Match.paginate(page: params[:page], per_page: 50)
+    @matches = Match.all
   end
 
-  def recent_matches
+  def show
+    set_match
   end
+
+  # def recent_matches
+  # end
+
+  def to_param
+    match_id
+  end
+
+  private
+
+  def set_match
+    @match = Match.find_by(match_id: params[:match_id])
+  end
+
 end
