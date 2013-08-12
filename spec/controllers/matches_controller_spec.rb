563d69606932ce84776cb3ec2639402fb8570449
theoretick <me@lucascharles.me>
2013-08-12 16:56:08 -0700

    added lots of spec scaffolding and pagination to match index

--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe MatchesController do
 
+  describe "GET 'index'" do
+
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+
+  end
+
+  # TEMP:
+  # it "returns empty array without param url" do
+  #   post :find_by_url
+  #   expect(response.body).to eq("[]")
+  # end
+
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
+# require 'simplecov'
+# SimpleCov.start 'rails'
+
+require 'rubygems'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+
+require 'capybara/rails'
+require 'webmock/rspec'
+require 'vcr'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -14,20 +23,11 @@ ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
   # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.mock_with :rspec
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.include Capybara::DSL
+  # Need to tell rspec to look for 'features' instead of 'requests' for Capybara2
+  config.include RSpec::Rails::RequestExampleGroup, type: :feature
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
