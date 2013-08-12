aa854b98af75b2327fab5fe928aa18b70ccf3583
Neil Northrop <nnorthrop@gmail.com>
2013-08-12 10:29:28 -0700

    fuck yeah

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,11 +2,11 @@ class MatchesController < ApplicationController
   require 'will_paginate/array'
 
   def show
-    @matches = Match.all.paginate(page: 1, per_page: 1)
+    @matches = Match.all.paginate(page: 1, per_page: 2)
   end
 
   def index
-    @matches = Match.all.paginate(page: 1, per_page: 1)
+    @matches = Match.all.paginate(page: 1, per_page: 2)
   end
 
   def recent_matches
