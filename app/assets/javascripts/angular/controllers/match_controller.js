98bf916ea1e9874e2d99ebe91f4dc9f84de8e20d
Greg Larrenaga <greglarrenaga@gmail.com>
2013-08-15 10:10:57 -0700

    Working on Hero Show pages. Changed some file names. Added some new files for the Hero Show Page.

--- app/assets/javascripts/angular/controllers/match_controller.js
+++ app/assets/javascripts/angular/controllers/match_controller.js
@@ -1,16 +1,21 @@
 app.controller('matchCtrl', function($scope, $http) {
 
-  $scope.matches = $http.get('/matches').success(function(data) {
-    $scope.matches = data;
-  });
+  //Rails send the page. With the match id as a param.
+  //Angular makes a JSON request for the match details.
 
-  $scope.getMatch = function(matchID) {
-    $http.get('/matches/' + matchID).success(function(data) {
-      console.log('success!');
-      $scope.showMatch = data;
-    });
-  };
 
-  // $scope.showMatch = function(){getMatch};
+
+  // $scope.matches = $http.get('/matches').success(function(data) {
+  //   $scope.matches = data;
+  // });
+
+  // $scope.getMatch = function(matchID) {
+  //   $http.get('/matches/' + matchID).success(function(data) {
+  //     console.log('success!');
+  //     $scope.showMatch = data;
+  //   });
+  // };
+
+  // // $scope.showMatch = function(){getMatch};
 
 });
