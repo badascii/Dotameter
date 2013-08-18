0b615e1dd2d18b77d3125ddd183a1c6ab0cdafa0
Greg Larrenaga <greglarrenaga@gmail.com>
2013-08-17 19:36:39 -0700

    It works and returns what I need. Now I need to make a controller method, a route, then some angular to scoop it up. then connect to D3 for graphing.

--- app/models/match.rb
+++ app/models/match.rb
@@ -25,4 +25,76 @@ class Match
 
   validates :match_id, presence: true, uniqueness: true
 
+
+
+
+  #{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
+  #
+  #
+  # I Stole this from active record...
+  # http://apidock.com/rails/Array/in_groups_of
+  #
+  #
+  #{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}
+  # File activesupport/lib/active_support/core_ext/array/grouping.rb, line 19
+  def in_groups_of(number, fill_with = nil)
+    if fill_with == false
+      collection = self
+    else
+      # size % number gives how many extra we have;
+      # subtracting from number gives how many to add;
+      # modulo number ensures we don't add group of just fill.
+      padding = (number - size % number) % number
+      collection = dup.concat([fill_with] * padding)
+    end
+
+    if block_given?
+      collection.each_slice(number) { |slice| yield(slice) }
+    else
+      groups = []
+      collection.each_slice(number) { |group| groups << group }
+      groups
+    end
+  end
+
+
+  # Returns a nice array of node objects from the Mongo DB for Heros.
+  # Looks like = [{'y' => height_of_node, 'runningSum' => num_games_won, 'runningTotal' => num_games_tot}]
+  def self.hero_win_graph(hero_id, groups_of)
+    # if groups_of == false
+    #   match_groups = 0
+    # end
+    matches = Match.all
+    runningSum = 0
+    runningTotal = 0
+    winPercent = 0.00
+    histogram = []
+    matches.in_groups_of(groups_of).each do |batch|
+      batch.each do |match|
+        # somewhere here this will need to be built in batches
+        # get the winning team/
+        winningTeam = if match.radiant_win == true then 'Radiant' else 'Dire' end
+        # Loop through all the players in that match looking for heroes that match the hero passed.
+        match.players.each do |player|
+          # if the hero in the match is the hero we are looking for
+          if (player['hero_id'] == hero_id)
+            # check if this player was on the winning team
+            playerTeam = DB.which_team(player['player_slot'])
+            if (playerTeam == winningTeam)
+              # add one to the running sum
+              runningSum += 1
+            end
+            # add one to the running total
+            runningTotal += 1
+          end
+        end
+        # calculate the win %
+        winPercent = runningSum.to_f / runningTotal.to_f
+        # push it onto the histogram array
+        histogram.push({'y' => winPercent, 'totMatches' => runningTotal, 'totWins' => runningSum})
+      end
+    end
+    return histogram
+  end
+
 end
