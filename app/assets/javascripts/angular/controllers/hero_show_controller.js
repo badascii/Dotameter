323d1a8182004a0626829556a000f75997bf600c
Greg Larrenaga <greglarrenaga@gmail.com>
2013-08-18 12:29:36 -0700

    Almost there.

--- app/assets/javascripts/angular/controllers/hero_show_controller.js
+++ app/assets/javascripts/angular/controllers/hero_show_controller.js
@@ -1,17 +1,18 @@
 app.controller('heroShowCtrl', function($scope, $http) {
 
   // Needs to call get '/hero/win_percent_histogram?hero_id='BLAH'
-  var getHeroGraph = function(hero_id) {
-    $http.get('/hero/win_percent_histogram?hero_id=' + ).success(function(data) {
-      console.log(data);
+  $scope.getHeroGraph = function(hero_id) {
+    $http.get('/hero/win_percent_histogram?hero_id=' + hero_id).success(function(data) {
+      $scope.heroWinPercent = data;
     }).error(function(data) {
-      console.log(data);
+      console.log("There was an error getting the Hero Graph from Rails." + data);
     });
-  }
+  };
 
 
 });
 
 $(document).ready( function() {
-
+  angular.element($('.hero-show')).scope().getHeroGraph(1590);
 });
+
