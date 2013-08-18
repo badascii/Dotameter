78c8ebb5a27c2c2d3ae78250fe5a867946474f91
Greg Larrenaga <greglarrenaga@gmail.com>
2013-08-18 12:10:43 -0700

    Adding show graph data to hero show page.

--- app/assets/javascripts/angular/controllers/hero_show_controller.js
+++ app/assets/javascripts/angular/controllers/hero_show_controller.js
@@ -1,6 +1,17 @@
 app.controller('heroShowCtrl', function($scope, $http) {
 
   // Needs to call get '/hero/win_percent_histogram?hero_id='BLAH'
+  var getHeroGraph = function(hero_id) {
+    $http.get('/hero/win_percent_histogram?hero_id=' + ).success(function(data) {
+      console.log(data);
+    }).error(function(data) {
+      console.log(data);
+    });
+  }
 
+
+});
+
+$(document).ready( function() {
 
 });
